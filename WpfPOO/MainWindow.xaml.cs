6083866f2a7cfd970e9c4f0693258e8664d5166d
ralph45 <ralphpsn2@mail.com>
2022-02-21 13:04:24 +0100

    WPF y POO

--- WpfPOO/MainWindow.xaml.cs
+++ WpfPOO/MainWindow.xaml.cs
@@ -23,6 +23,30 @@ namespace WpfPOO
         public MainWindow()
         {
             InitializeComponent();
+            
+            Label l = new Label();
+            l.Content = "Email*";
+            
+            TextBox t = new TextBox();
+            t.Text = "carlos.igformacion@gmail.com";
+            t.Height = 20;
+            t.Width = 50;
+
+            
+            principal.Children.Add(t);
+
+            Button btn = new Button();
+            btn.Content = "Abrir";
+            btn.VerticalAlignment = VerticalAlignment.Top;
+
+            l.Background = Brushes.Chocolate;
+
+            Persona p = new Persona("Carlos", 27);
+            principal.Children.Add(p.Mostrar());
+
+            Persona p2 = new Persona("PEPE", 30);
+            principal.Children.Add(p2.Mostrar());
+
         }
     }
 }
---
+++ WpfPOO/Persona.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WpfPOO
+{
+    public class Persona
+    {
+        private string nombre;
+        private int edad;
+
+        public Persona(string nombre, int edad)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+        }
+
+        public Grid Mostrar()
+        {
+            Label l = new Label();
+            l.Content = "Nombre: " + nombre;
+
+            Label edad = new Label();
+            edad.Content = "Edad: " + this.edad;
+            edad.HorizontalAlignment = HorizontalAlignment.Right;
+
+            Grid g = new Grid();
+            g.Children.Add(l);
+            g.Children.Add(edad);
+
+            return g;
+        }
+    }
+}
